7d0ced57b198621d9d45b17c6de84359d9d24d82
Robert Burnat <burnat.robertt@gmail.com>
2017-07-26 11:21:51 +0200

    Add CORS

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+$(function() {
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-var tweetText = 'Quote of the day - ' + quoteText + 'Author: ' + quoteAuthor;
-
-functon getQuote() {
-	$.getJSON(quoteUrl, createTweet);
+function getQuote() {
+	$.getJSON(prefix + quoteUrl, createTweet);
+	$.ajaxSetup({ cache: false});
 }
 // OR //
 
@@ -21,11 +22,18 @@ function createTweet(input) {
 	if(!quoteAuthor.length) {
 		quoteAuthor = "Unknown author";
 	}
+	var tweetText = 'Quote of the day - ' + quoteText + 'Author: ' + quoteAuthor;
 	if(tweetText.length > 140) {
 	getQuote();
 	}else {
 		var tweet = tweetLink + encodeURIComponent(tweetText);
-		$('.quote').text(quoteText)
+		$('.quote').text(quoteText);
+		$('.author').text('Author:' + quoteAuthor);
+		$('.tweet').attr('href', tweet);
 	}
 }
-
+getQuote();
+$(.trigger).click(function() {
+	getQuote();
+	});
+});
