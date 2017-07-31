74aa41f090d63397d26b20b17dff325cb52f4c09
Robert Burnat <burnat.robertt@gmail.com>
2017-07-31 12:11:47 +0200

    mega fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,30 @@
 $(() => {
-	let prefix = "https://cors-anywhere.herokuapp.com/";
-	let tweetLink = "https://twitter.com/intent/tweet?text=";
-	let quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-	
-	function getQuote() {
-		$.getJSON(prefix + quoteUrl, createTweet);
-		$.ajaxSetup({ cache: false});
-	}
+let prefix = "https://cors-anywhere.herokuapp.com/";
+let tweetLink = "https://twitter.com/intent/tweet?text=";
+let quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+function getQuote() {
+	$.getJSON(prefix + quoteUrl, createTweet);
+	$.ajaxSetup({ cache: false});
+}
+// OR //
 
-	function createTweet(input) {
-		let data = input[0];
-		let quoteText = $(data.content).text().trim();
-		let quoteAuthor = data.title;
-		if(!quoteAuthor.length) {
-			quoteAuthor = "Unknown author";
-		}
-		let tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
-		if (tweetText.length > 140) {
-			getQuote();
-		} else {
-			let tweet = tweetLink + encodeURIComponent(tweetText);
-			$('.quote').text('“'+quoteText+'”');
-			$('.author').text('Author: ' + quoteAuthor);
-			$('.tweet').attr('href', tweet);
-		}
+// $.ajax({
+// 	dataType: 'json',
+// 	url: prefix + quoteUrl,
+// 	data: null,
+// 	success: createTweet
+// });
+
+function createTweet(input) {
+	let data = input[0];
+	let quoteText = $(data.content).text().trim();
+	let quoteAuthor = data.title;
+	if(!quoteAuthor.length) {
+		quoteAuthor = "Unknown author";
 	}
-	
+	let tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
+	if(tweetText.length > 140) {
 	getQuote();
-<<<<<<< HEAD
 	}else {
 		let tweet = tweetLink + encodeURIComponent(tweetText);
 		$('.quote').text('“'+quoteText+'”');
@@ -37,7 +34,4 @@ $(() => {
 }
 getQuote();
 $('.trigger').click(() => getQuote());
-=======
-	$('.trigger').click(() => getQuote());
->>>>>>> 1317b221323f3c103312f38bca568e98266aea1f
 });
